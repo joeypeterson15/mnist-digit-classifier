c2f3009cae85dd539c4681328853f2c141cc5f58
Joey Peterson <joey.peterson15@gmail.com>
2024-10-25 18:39:54 -0700

    mess around

--- index.py
+++ index.py
@@ -1,6 +1,16 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import torch
+
+weights = torch.ones(4, requires_grad=True)
+
+print('weights:', weights)
+
+model_output = (weights*3).sum()
+model_output2 = model_output*2
+
+model_output.backward()
+# model_output2.backward()
+print('model output', model_output)
+print('model output2', model_output2)
+
+print(weights.grad)
+# for epoch in range(3):
