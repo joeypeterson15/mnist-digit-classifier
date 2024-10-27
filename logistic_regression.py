be8e4090b3fee54ec2650758a83d7d26ca697bc0
Joey Peterson <joey.peterson15@gmail.com>
2024-10-27 12:11:58 -0700

    use mnist dataset, create feed forward model

--- logistic_regression.py
+++ logistic_regression.py
@@ -8,21 +8,102 @@
 import torch
 import torch.nn as nn
 import numpy as np
-from sklearn import datasets
-from sklearn.preprocessing import StandardScaler #scale our features
-from sklearn.model_selection import train_test_split
-# 0) prepare data
-bc = datasets.load_breast_cancer()
-X, y = bc.data, bc.target
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib as plt
 
-n_samples, n_features = X.shape
-print(f'samples/features: {n_samples} {n_features}')
+# device config
+device  = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+# hyper parameters
+input_size = 784 # 28x28
+hidden_size = 100
+num_classes = 10
+num_epochs = 2
+batch_size = 100
+learning_rate = 0.001
 
-#scale
-sc = StandardScaler()
+# MNIST
+train_dataset = torchvision.datasets.MNIST(root='./data', train=True,
+                                           transform= transforms.ToTensor(), download=True)
 
-# 1) setup model
-# 2) setup loss and optimizer
-# 3) training loop
+test_dataset = torchvision.datasets.MNIST(root='./data', train=False,
+                                          transform= transforms.ToTensor())
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size,
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size,
+                                           shuffle=False)
+examples = iter(train_loader)
+# print(examples)
+
+samples, labels = next(examples)
+print(samples.shape, labels.shape)
+# print(train_loader)
+
+# loss algorithm and activation functions
+# good activation function is softmax
+# A good loss algorithm is the cross function
+
+class digitClassifier(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(digitClassifier, self).__init__()
+        self.layer1 = nn.Linear(input_size, hidden_size)
+        # use softmax activation function inbetween multi class network layers
+        self.softmax = nn.Softmax()
+        self.layer2 = nn.Linear(hidden_size, num_classes)
+        # use cross entropy loss function
+        # self.loss = nn.CrossEntropyLoss() # applies softmax
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.softmax(out)
+        out = self.layer2(out)
+        # out = self.loss(out)
+        return out
+
+model = digitClassifier(input_size, hidden_size, num_classes)
+
+# need loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+n_total_steps = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        # images shape: [100, 1, 28, 28]. 100 images with grayscale(1 channel) with 784(28x28) inputs
+
+        images = images.reshape(-1, 28*28).to(device)
+        labels = labels.to(device)
+
+        # forward
+        pred = model(images)
+        loss = criterion(pred, labels)
+
+        # backwards
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i + 1) % 100 == 0:
+            print(f'epoch {epoch + 1} / {num_epochs}, step {i + 1}/{n_total_steps}, loss = {loss.item():.4f}')
+
+# test
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    for images, labels in test_loader:
+        images = images.reshape(-1, 28*28).to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+
+        # max will give value, index. We want index(predicitions)
+        _, predictions = torch.max(outputs, 1)
+        n_samples += labels.shape[0]
+        n_correct += (predictions == labels.sum().item())
+    
+    acc = 100.0 * n_correct / n_samples
+    print(f'accuracy={acc}')
+
+
